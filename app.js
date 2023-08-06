0300ad361649bfcc9a6af2c3fdcc77e5f5ed3f08
7SOMAY <ssomay2002@gmail.com>
2023-08-06 07:28:07 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import {config} from "dotenv";
+
 config({
     path: "./vars/.env",
 });
@@ -14,15 +16,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 
 // Import Routes
 import room from './routes/roomRoutes.js';
 import user from './routes/userRoutes.js';
 
-app.use("/api/v1/",room);
-app.use("/api/v1/",user);
+app.use("/api/v1/", room);
+app.use("/api/v1/", user);
 
 export default app;
 
