f7b34e403079e284af63a5fac63642c593bef1b2
7SOMAY <ssomay2002@gmail.com>
2023-08-07 16:52:42 +0530

    update

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
+    // --------------AFTER DEPLOYMENT---------------
     origin: "https://home-automator.vercel.app",
+
+    // -----------------DEVELOPMENT-----------------
+    // origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
